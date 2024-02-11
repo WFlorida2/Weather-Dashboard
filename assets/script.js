d78fadbcb66678de1f46e206e29f33b14c40ea2e
Amr Shehata <amr_shehata@hotmail.com>
2024-02-11 21:44:58 +0000

    Added footer with social media that I used in portofolio project early in the course, played around with more styling, and corrected search form

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+            // Show the modal on page load
+    $('#infoModal').modal('show');
     // Function to update the current date and time
     function updateDateTime() {
         // Get the current date and time using the dayjs library and format it
@@ -15,85 +17,100 @@ $(document).ready(function() {
 
     // declaring API key
     var APIKey = "f848cebd8a210709c011b878be93124e";
-    var queryCurrent = `https://api.openweathermap.org/data/2.5/weather?q=Ustka&appid=${APIKey}`;
 
-    // Fetching data for current day
-    fetch(queryCurrent)
-    .then(function (result) {
-        return result.json();
-    })
-    .then(function (data) {
-        var cityName = data.name;
-        var country = data.sys.country; // added a country identifier as some countries have same city names
-        var currentDate = dayjs().format("("+"DD/MM/YYYY"+")");
-        var weatherIcon = data.weather[0].icon;
-        // ?: This is the ternary operator's conditional part. It's like saying "if data.rain exists and is truthy, then..." // Credit to google search on alternative 
-        var rainData = data.rain ? data.rain['1h'] : 0; // if not rainy then shows zero chance of getting wet 🤣 👍
-        var tempC = data.main.temp - 273.15;
-        var wind = data.wind.speed;
-        var humidity = data.main.humidity;
-        var cardHTML = `
-            <div id="currentDay-card" class="card">
-                <div class="card-body">
-                <h5 class="card-title">
-                <span style="font-weight: bold;">${cityName}</span> , 
-                <span style="font-weight: bold;">${country}</span>
-                <span style="font-weight: bold;">${currentDate}</span>
-                <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon"></h5>
-                <p class="card-text">Rain: ${rainData} mm</p>
-                <p class="card-text">Temp: ${tempC.toFixed(2)} °c</p>
-                <p class="card-text">Wind: ${wind} km/h</p>
-                <p class="card-text">Humidity: ${humidity} %</p>
-                </div>
-            </div>
-        `;
-        // Appending the card to the container with ID 'today'
-        $('#today').html(cardHTML);
-    });
-
-    var queryForecast = `https://api.openweathermap.org/data/2.5/forecast?q=Ustka&appid=${APIKey}`;
-
-    // Fetching data forecast for the coming 5 days
-    fetch(queryForecast)
-    .then(function (result) {
-        return result.json();
-    })
-    .then(function (forecastData) {
-        var forecastList = forecastData.list;
-        // looping through the list
-        var forecastHTML = '';
+    // Event listener for the search button
+    $('#search-button').click(function() {
+        // Get the value entered by the user in the input field
+        var cityName = $('#input-field').val().trim();
         
-        var currentDate = dayjs().format("DD/MM/YYYY"); // Current date
-        var addedDates = []; // Array to store dates already added to the forecast HTML
-        var wind, humidity; // Declaring wind and humidity variables
-
-        // Using traditional for loop instead of forEach
-        for (var i = 0; i < forecastList.length; i++) {
-            var forecast = forecastList[i];
-            var date = dayjs.unix(forecast.dt).format("DD/MM/YYYY");
-            // Exclude forecast for the current day and duplicates
-            if (date !== currentDate && !addedDates.includes(date)) {
-                addedDates.push(date); // Add the date to the array
-                var weatherIcon = forecast.weather[0].icon;
-                var tempC = forecast.main.temp - 273.15;
-                wind = forecast.wind.speed; // Assigning wind speed
-                humidity = forecast.main.humidity; // Assigning humidity
-                forecastHTML += `
-                    <div class="col">
-                        <div id="forecast-card" class="card mb-2 mb-sm-2 mx-lg-3 text-center">
-                            <div class="card-body">
-                                <h5 class="card-title">${date}</h5>
-                                <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
-                                <p class="card-text">Temp: ${tempC.toFixed(2)} °c</p>
-                                <p class="card-text">Wind: ${wind} km/h</p>
-                                <p class="card-text">Humidity: ${humidity} %</p>
-                            </div>
+        // Check if the input field is not empty
+        if (cityName !== "") {
+            // Construct the API query URL for current weather data
+            var queryCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`;
+            
+            // Fetching data for current day
+            fetch(queryCurrent)
+            .then(function (result) {
+                return result.json();
+            })
+            .then(function (data) {
+                var cityName = data.name;
+                var country = data.sys.country; // added a country identifier as some countries have same city names
+                var currentDate = dayjs().format("("+"DD/MM/YYYY"+")");
+                var weatherIcon = data.weather[0].icon;
+                // ?: This is the ternary operator's conditional part. It's like saying "if data.rain exists and is truthy, then..." // Credit to google search on alternative 
+                var rainData = data.rain ? data.rain['1h'] : 0; // if not rainy then shows zero chance of getting wet 🤣 👍
+                var tempC = data.main.temp - 273.15;
+                var wind = data.wind.speed;
+                var humidity = data.main.humidity;
+                var cardHTML = `
+                    <div id="currentDay-card" class="card">
+                        <div class="card-body">
+                        <h5 class="card-title">
+                        <span style="font-weight: bold;">${cityName}</span> , 
+                        <span style="font-weight: bold;">${country}</span>
+                        <span style="font-weight: bold;">${currentDate}</span>
+                        <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon"></h5>
+                        <p class="card-text">Rain: ${rainData} mm</p>
+                        <p class="card-text">Temp: ${tempC.toFixed(2)} °c</p>
+                        <p class="card-text">Wind: ${wind} km/h</p>
+                        <p class="card-text">Humidity: ${humidity} %</p>
                         </div>
                     </div>
                 `;
-            }
+                // Appending the card to the container with ID 'today'
+                $('#today').html(cardHTML);
+            });
+
+            // Construct the API query URL for forecast data
+            var queryForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`;
+            
+            // Fetching data forecast for the coming 5 days
+            fetch(queryForecast)
+            .then(function (result) {
+                return result.json();
+            })
+            .then(function (forecastData) {
+                var forecastList = forecastData.list;
+                // looping through the list
+                var forecastHTML = '';
+                
+                var currentDate = dayjs().format("DD/MM/YYYY"); // Current date
+                var addedDates = []; // Array to store dates already added to the forecast HTML
+                var wind, humidity; // Declaring wind and humidity variables
+
+                // Using traditional for loop instead of forEach
+                for (var i = 0; i < forecastList.length; i++) {
+                    var forecast = forecastList[i];
+                    var date = dayjs.unix(forecast.dt).format("DD/MM/YYYY");
+                    // Exclude forecast for the current day and duplicates
+                    if (date !== currentDate && !addedDates.includes(date)) {
+                        addedDates.push(date); // Add the date to the array
+                        var weatherIcon = forecast.weather[0].icon;
+                        var tempC = forecast.main.temp - 273.15;
+                        wind = forecast.wind.speed; // Assigning wind speed
+                        humidity = forecast.main.humidity; // Assigning humidity
+                        forecastHTML += `
+                            <div class="col">
+                                <div id="forecast-card" class="card mb-2 mb-sm-2 mx-lg-3 text-center">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${date}</h5>
+                                        <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
+                                        <p class="card-text">Temp: ${tempC.toFixed(2)} °c</p>
+                                        <p class="card-text">Wind: ${wind} km/h</p>
+                                        <p class="card-text">Humidity: ${humidity} %</p>
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+                    }
+                }
+            
+                $('#forecast').html(`<div class="row">${forecastHTML}</div>`);
+            });
+        } else {
+            // If city name is empty, display the modal
+            $('#infoModal').modal('show');
         }
-    
-        $('#forecast').html(`<div class="row">${forecastHTML}</div>`);
     });
 });
