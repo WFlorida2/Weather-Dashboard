19bb2ab54ba1f87676dcea2a8ac7ae587b457616
Amr Shehata <amr_shehata@hotmail.com>
2024-02-11 01:51:38 +0000

    added code to show city name, country ID , date next to it and a current condition icon

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+    $(document).ready(function() {
+
     // Function to update the current date and time
     function updateDateTime() {
         // Get the current date and time using the dayjs library and format it
@@ -9,18 +11,31 @@
         updateDateTime();
         // Update the date and time every second (1000 milliseconds)
         setInterval(updateDateTime, 1000);
-
-    // This is Weather API key
-    var APIKey = "f848cebd8a210709c011b878be93124e";
-
-    // Here building the URL I need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +"q=Cairo,Egypt&appid=" + APIKey;
-    console.log(queryURL);
-    // Here I run a Fetch call to the OpenWeatherMap API
-    fetch(queryURL)
-    .then(function (result) {
+        
+        // declaring API key
+        var APIKey = "f848cebd8a210709c011b878be93124e";
+        var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=Ustka&appid=${APIKey}`;
+        fetch(queryURL)
+        .then(function (result) {
         return result.json();
-    })
-    .then(function (data) {
-    console.log(data);
+        })
+        .then(function (data) {
+        
+        var cityName = data.name;
+        var country = data.sys.country; // added a country identifier as some countries have same city names
+        var currentDate = dayjs().format("("+"DD/MM/YYYY"+")");
+        var weatherIcon = data.weather[0].icon;
+        console.log(weatherIcon);
+        var cardHTML = `
+            <div class="card">
+                <div class="card-body">
+                <h5 class="card-title">${cityName +" , "+country} ${currentDate} 
+                <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon"></h5>
+                </div>
+            </div>
+        `;
+        // Appending the card to the container with ID 'today'
+        $('#today').html(cardHTML);
     });
+});
+
